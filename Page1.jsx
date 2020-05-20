8d883b20cba150c0aeddc14b635751ce205c3bce
vdeepakrvs <vdeepakrvs@gmail.com>
2020-05-20 14:54:59 +0530

    2 files modified

--- Page1.jsx
+++ Page1.jsx
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-function ListOfItems(props){
-  return(
+function ListOfItems(props) {
+  return (
     <div className="parent-div">
-    <div className="row">
-      <div className="individual-item">     
-        <img src={props.item.pic} alt={props.item.text} class="img-responsive"/>
+      <div className="row">
+        <div className="individual-item">
+          <img
+            src={props.item.pic}
+            alt={props.item.text}
+            class="img-responsive"
+          />
+        </div>
+      </div>
+      <div className="row desc">
+        <div className="text-class">
+          <h2>
+            {props.item.material} - {props.item.price}
+          </h2>
+          <h1 className="text-h1">{props.item.text}</h1>
+        </div>
+      </div>
+      <div>
+        <button caption="Add to Cart">Add To Cart</button>
       </div>
     </div>
-    <div className="row desc">
-      <div className="text-class">       
-        <h2>{props.item.material} - {props.item.price}</h2>
-         <h1 className="text-h1">{props.item.text}</h1>
-      </div>
-    </div>
-    <div>
-    <button caption=""></button>
-    </div>
-    </div>
-
-  )
+  );
 }
 
-export default ListOfItems
+export default ListOfItems;
